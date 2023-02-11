fd889c78a5cc6895196dd73e7cd87ae6d1261596
mariolarios <108195141+mariolarios@users.noreply.github.com>
2023-02-11 14:04:42 -0500

    put jwt token in cookies

---
+++ src/app.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+///applies try and catch to all controllers automatically
+require("express-async-errors");
+
+///import express
+const express = require("express");
+const app = express();
+
+///Rest of the packages
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+///set up database
+const connectDB = require("./db/connect");
+
+/// routers
+const authRouter = require("./routes/authRoutes");
+
+/// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+/// This lets me see what routes I visited on the browser
+/// it also gives me the status code.
+app.use(morgan("tiny"));
+///express json
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+///routes/requests
+app.get("/", (req, res) => {
+  res.send("Food-Hub");
+});
+
+app.use("/api/v1/auth", authRouter);
+
+///use middleware...this is placed after the routes
+///because it runs to see if the routes exist before executing the middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+/// set up port
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ src/controllers/authController.js
@@ -0,0 +1,50 @@
+/// this controller is just going to deal with the auth part of the app
+/// that way it stays better organized as the project gets bigger.
+
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const { createJWT } = require("../utils");
+
+/// REGISTER
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  /// first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: tokenUser });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+};
+
+///LOGIN
+const login = async (req, res) => {
+  res.send("login user");
+};
+
+///LOGOUT
+const logout = async (req, res) => {
+  res.send("log out user");
+};
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
